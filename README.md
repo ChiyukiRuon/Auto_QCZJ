253f58ddb870fc3d27d09f3e03f97d09ef153d5f
ChiyukiRuon <1275846107@qq.com>
2022-11-17 13:12:11 +0800

    使用yaml作为多用户配置文件，修改了readme，添加了云函数部署教程

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
-# Fuck_QCZJ
+# Auto_QCZJ
+适用于云函数部署的青年大学习自动打卡，**仅限浙江地区使用**  
+原项目地址[WuliAPO/Fuck_QCZJ](https://github.com/WuliAPO/Fuck_QCZJ)
 
-青春浙江-青年大学习打卡，仅浙江地区
+## 功能
+* 可用于多用户打卡
+* 适用于云函数部署
 
-## 使用方法
-1、打开main.py
+## 使用方法（以腾讯云为例）
+* ### Windows
+1. [下载](https://github.com/ChiyukiRuon/Auto_QCZJ/archive/refs/heads/main.zip)压缩包，解压后进入项目文件夹
+2. 进入config文件夹，复制`config.yaml.example`并**删除`.example`后缀**，你可以任意修改文件名，脚本会自动读取后缀名为`yaml`的配置文件
+3. 打开你的yaml配置文件，修改**nid**、**cardNo**、**openid**，其中nid和openid需要自己抓包
+4. 返回上一级文件夹，在文件夹内右键，选择`在终端中打开`
+5. 在终端内运行`pip install -r requirements.txt -t .`
+6. 打开[腾讯云函数控制台](https://console.cloud.tencent.com/scf)，点击`新建`，选择`从头开始`
+7. **函数名称**可以自定义，**其余保持默认**
+8. **函数代码**选择`本地上传文件夹`，选择刚刚的文件夹上传
+9. 在**高级配置**内的**环境配置**，`执行超时时间`填写30
+10. **触发器配置**选择`自定义创建`-**触发周期**选择`自定义触发周期`-**Cron表达式**填写`0 0 22 * * MON *`，表示在每周一的22：00：00执行本函数
+11. **同意协议**并点击**完成**即可成功部署
+> 关于更多Cron表达式的信息，可以参考腾讯云的 [定时触发器说明](https://cloud.tencent.com/document/product/583/9708)  
 
-2、修改nid、cardNo、openid，其中nid和openid需要自己抓包
+> 抓包教程可以参考原项目内的链接
 
-3、运行即可
-
-nid是团组织的代号
-cardNo是打卡用的昵称
-openid是微信分配给你的识别码
-
---------
-抓包教程 https://blog.csdn.net/qq_41742289/article/details/127157589
-
-# 求Star
-顺路问一下，有没有办法，在不抓包的情况下获取到自己的openid？
-比如写一个微信小程序，里面引入青春浙江公众号的授权按钮，从而获取到openid，这个方法是否可行？
---
+++ index.py
@@ -0,0 +1,107 @@
+import os
+import json
+import yaml
+import time
+import requests
+
+
+# 获取AccessToken
+def getAccessToken(session, openid):
+    time_stamp = str(int(time.time()))  # 获取时间戳
+    url = "https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/login/we-chat/callback?callback=https%3A%2F%2Fqczj.h5yunban.com%2Fqczj-youth-learning%2Findex.php&scope=snsapi_userinfo&appid=wx56b888a1409a2920&openid=" + openid + "&nickname=ZhangSan&headimg=&time=" + time_stamp + "&source=common&sign=&t=" + time_stamp
+    res = session.get(url)
+    access_token = res.text[45:81]  # 比较懒，直接截取字符串了
+    print("获取到AccessToken:", access_token)
+    return access_token
+
+
+# 获取当前最新的课程代号
+def getCurrentCourse(session, access_token):
+    url = "https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/common-api/course/current?accessToken=" + access_token
+    res = session.get(url)
+    res_json = json.loads(res.text)
+    if (res_json["status"] == 200):  # 验证正常
+        print("获取到最新课程代号:", res_json["result"]["id"])
+        return res_json["result"]["id"]
+    else:
+        print("获取最新课程失败！")
+        print(res.text)
+        exit(0)
+        return None
+
+
+# 签到 成功返回True，失败返回False
+def getJoin(session, access_token, current_course, nid, cardNo):
+    data = {
+        "course": current_course,  # 大学习期次的代码，如C0046，本脚本已经帮你获取啦
+        "subOrg": None,
+        "nid": nid,  # 团组织编号，形如N003************
+        "cardNo": cardNo  # 打卡昵称
+    }
+    url = "https://qczj.h5yunban.com/qczj-youth-learning/cgi-bin/user-api/course/join?accessToken=" + access_token
+    res = session.post(url, json=data)  # 特别注意，此处应选择json格式发送data数据
+    print("签到结果:", res.text)
+    res_json = json.loads(res.text)
+    if res_json["status"] == 200:  # 验证正常
+        print("’{}‘似乎签到成功了".format(cardNo))
+        return True
+    else:
+        print("’{}‘签到失败！".format(cardNo))
+        exit(0)
+
+
+# 查找config下的配置文件
+def find_config():
+    file_name = []
+    for file in os.listdir('./config'):
+        if os.path.splitext(file)[1] == '.yaml':
+            file_name.append(file)
+
+    return file_name
+
+
+def main():
+    yaml_file = find_config()
+    print("共获取到{}个配置文件：{}".format(len(yaml_file), yaml_file))
+    for i in range(len(yaml_file)):
+        with open('./config/{}'.format(yaml_file[i]), 'r', encoding='utf-8') as f:
+            file = f.read()
+            yaml_data = yaml.safe_load(file)
+            print(yaml_data)
+            if yaml_data['enable']:
+                openid = yaml_data['openid']
+                cardNo = yaml_data['cardNo']
+                nid = yaml_data['nid']
+                print("正在为用户’{}‘签到".format(cardNo))
+
+                session = requests.session()
+                session.headers = {
+                    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F70 Safari/600.1.4'
+                }
+
+                # 获取token
+                time.sleep(2)
+                access_token = getAccessToken(session, openid)
+
+                # 获取最新的章节
+                time.sleep(2)
+                current_course = getCurrentCourse(session, access_token)
+
+                # 签到
+                time.sleep(2)
+                getJoin(session, access_token, current_course, nid, cardNo)
+
+                time.sleep(2)
+            else:
+                print("配置文件’{}‘未启用!".format(yaml_file[i]))
+                pass
+
+        f.close()
+
+
+def main_handler(event, context):
+    return main()
+
+
+if __name__ == '__main__':
+    main()
